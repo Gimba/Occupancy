32f7a63c3a119a64eb93560bf574e5ae4237e760
Martin Rosellen <Martin.Rosellen@fu-berlin.de>
2017-09-26 17:35:16 +0100

    implementation of input list tests

--- input_parser.py
+++ input_parser.py
@@ -20,6 +20,8 @@ class Inputs():
 
                 try:
                     self.mutation = int(argv[i + 1])
+                    if self.mutation < 0:
+                        raise ValueError("Mutation argument has to be a positive number. Given: %s" % argv[i + 1])
                 except ValueError:
                     raise ValueError("Argument following -r not castable to int. Given: %s" % argv[i + 1])
 
@@ -43,3 +45,30 @@ class Inputs():
 
         if self.input == []:
             raise IOError("-i flag not found.")
+
+        ## check input list ##
+        if len(self.input) % 2 == 1:
+            raise IOError("Every topology has to be followed by a trajectory, therefore the number of inputs has to be"
+                          "even. Given inputs: %d" % len(self.input))
+
+        # check topologies
+        if ".prmtop" not in self.input[0]:
+            raise IOError("List of inputs has to start with a topology (prmtop) file")
+
+        for i in range(0, len(self.input) - 2):
+            if ".prmtop" in self.input[i]:
+                if ".prmtop" not in self.input[i + 2]:
+                    raise IOError(
+                        "Every second element in the input list has to be of type prmtop. Given %s" % self.input)
+
+        # check trajectories
+        if not any(ext in self.input[1] for ext in [".inpcrd", ".nc", ".mdcrd", ".rst"]):
+            raise IOError(
+                "Second input has to be a trajectory file (.nc, .inpcrd, .rst, .mdrcd). Given %s" % self.input[1])
+
+        for i in range(1, len(self.input) - 2):
+            if any(ext in self.input[i] for ext in [".inpcrd", ".nc", ".mdcrd", ".rst"]):
+                if not any(ext in self.input[i + 2] for ext in [".inpcrd", ".nc", ".mdcrd", ".rst"]):
+                    raise IOError(
+                        "Every second element in the input list has to be a topology file (.nc, .inpcrd, .rst,"
+                        " .mdrcd). Given %s" % self.input)
