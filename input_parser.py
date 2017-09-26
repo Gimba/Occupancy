35aad59f352b221d5eb54a921126ce476ed8f492
Martin Rosellen <Martin.Rosellen@fu-berlin.de>
2017-09-26 15:51:16 +0100

    added tests for argument amount

--- input_parser.py
+++ input_parser.py
@@ -2,5 +2,6 @@ import argparse
 
 class InputParser():
     def __init__(self, argv):
+        self.argv = argv
         if not isinstance(argv,list):
             raise TypeError("Input has to be of type list. Given: %s" % type(argv))
--- test/test_input_parser.py
+++ test/test_input_parser.py
@@ -4,7 +4,7 @@ from input_parser import InputParser
 class TestInputParser(unittest.TestCase):
 
     def test_init_success(self):
-        result = InputParser(['test'])
+        result = InputParser(["1","2"])
         self.assertIsInstance(result, object)
 
     def test_init_fail(self):
@@ -15,6 +15,19 @@ class TestInputParser(unittest.TestCase):
 
     def test_init_with_string(self):
         self.assertRaises(TypeError, InputParser, "string")
+
+    def test_init_to_few_arguments(self):
+        self.assertRaises(TypeError, InputParser, [])
+
+    def test_init_no_arguments(self):
+        self.assertRaises(TypeError, InputParser, [])
+
+    def test_init_to_few_arguments(self):
+        self.assertRaises(TypeError, InputParser, ["1"])
+
+    def test_init_to_many_arguments(self):
+        self.assertRaises(TypeError, InputParser, ["1", "2", "3", "4", "5", "6"])
+
 
 if __name__ == '__main__':
     unittest.main()
