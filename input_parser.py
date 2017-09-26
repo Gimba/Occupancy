fabb4701103282df6ce31f414ded26dc4ecb7f0b
Martin Rosellen <Martin.Rosellen@fu-berlin.de>
2017-09-26 15:57:44 +0100

    implemented test cases

--- input_parser.py
+++ input_parser.py
@@ -5,3 +5,9 @@ class InputParser():
         self.argv = argv
         if not isinstance(argv,list):
             raise TypeError("Input has to be of type list. Given: %s" % type(argv))
+
+        if len(argv) < 2:
+            raise TypeError("At least two arguments needed. Given: %d" % len(argv))
+
+        if len(argv) > 5:
+            raise TypeError("A maximum of 5 arguments is allowed. Given: %d" % len(argv))
