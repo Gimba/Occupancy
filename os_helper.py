0ac81b42e2845ba8797f8ae49ff28e1d23072177
Martin Rosellen <Martin.Rosellen@fu-berlin.de>
2017-09-27 10:41:28 +0100

    removed unnecessary try and catch

--- os_helper.py
+++ os_helper.py
@@ -2,18 +2,12 @@ import os
 
 
 def create_output_folder(folder_name):
-    try:
-        if not os.path.exists(folder_name):
-            os.makedirs(folder_name)
-    except OSError:
-        raise OSError("Could not create folder %s" % folder_name)
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
 
 
 def copy_to_folder(f, folder):
-    try:
-        os.system("cp " + f + " " + folder)
-    except OSError:
-        raise OSError("Could not copy %s to folder %s" % f % folder)
+    os.system("cp " + f + " " + folder)
 
 
 def change_to_folder(folder):
